a1ea86561b8207d5284d7a565a09ac1d62ad9600
MohithMohan0708 <mohithmohan0708@gmail.com>
2025-02-14 18:46:44 +0530

    feat: Add Cart and PlaceOrder components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react'
 import Navbar from './components/Navbar/Navbar.jsx'
 import Home from './pages/Home/Home.jsx'
+import Cart from './pages/Cart/Cart.jsx'
+import PlaceOrder from './pages/PlaceOrder/PlaceOrder.jsx'
 import { Route, Routes } from 'react-router-dom'
 import Footer from './components/Footer/Footer.jsx'
 import LoginPopup from './components/LoginPopup/LoginPopup.jsx'
@@ -13,6 +15,8 @@ const App = () => {
         <Navbar setshowLogin={setshowLogin} ></Navbar>
         <Routes>
           <Route path='/' element={<Home />}></Route>
+          <Route path='/cart' element={<Cart />}></Route>
+          <Route path='/order' element={<PlaceOrder />}></Route>
         </Routes>
       </div>
       <Footer />
